b69d82f0811b7b9143769e86a7ba6ccd83df8632
Rajesh N <nrh27magnum@gmail.com>
2026-01-17 01:13:53 +0530

    feat(scheduler): add priority queue for task ordering

---
+++ internal/scheduler/priority_queue.go
@@ -0,0 +1,34 @@
+package scheduler
+
+import "container/heap"
+
+type Item struct {
+	WorkerID string
+	Score    float64
+}
+
+type PriorityQueue []Item
+
+func (pq PriorityQueue) Len() int { return len(pq) }
+func (pq PriorityQueue) Less(i, j int) bool {
+	return pq[i].Score < pq[j].Score
+}
+func (pq PriorityQueue) Swap(i, j int) {
+	pq[i], pq[j] = pq[j], pq[i]
+}
+func (pq *PriorityQueue) Push(x interface{}) {
+	*pq = append(*pq, x.(Item))
+}
+func (pq *PriorityQueue) Pop() interface{} {
+	old := *pq
+	n := len(old)
+	item := old[n-1]
+	*pq = old[:n-1]
+	return item
+}
+
+func NewPQ() *PriorityQueue {
+	pq := &PriorityQueue{}
+	heap.Init(pq)
+	return pq
+}
